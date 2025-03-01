6662e5b2b9d622074e4f158f1af67980befb05bf
prathammodi333 <prathammodi333@gmail.com>
2025-03-01 09:54:38 +0530

    fixed permissions

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,12 +2,14 @@
 # exit on error
 set -o errexit
 
-curl -fsSL https://deb.nodesource.com/setup_16.x | bash -  # Installs Node.js
-apt-get install -y nodejs
+node -v
+npm -v
 gem install rails -v 8.0.1
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
+bundle exec rails db:migrate
 
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
